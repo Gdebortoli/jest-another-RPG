2d161c8aaad2765f7b8558d5685083c281c4cab0
“Gigi <debortolg@gmail.com>
2022-11-21 01:06:25 -0800

    added getStats and getInventory functions + use prototype method

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -5,8 +5,6 @@ const Potion = require('../lib/Potion');
 // Replaces constructor's implementation with our faked data
 jest.mock('../lib/Potion');
 
-console.log(new Potion());
-
 // Test 1
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -16,7 +14,26 @@ test('creates a player object', () => {
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
   //Created Player Object to inventory
-    expect(player.inventory).toEqual(
-      expect.arrayContaining([expect.any(Object)])
-    );
+    expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
   });
+
+  // Test 2 - Checking player.getStatus() and make sure it returns obj w 4 properties
+test("gets the player's stats as an object", () => {
+  const player = new Player('Dave'); 
+
+  expect(player.getStats()).toHaveProperty('potions');
+  expect(player.getStats()).toHaveProperty('health');
+  expect(player.getStats()).toHaveProperty('strength');
+  expect(player.getStats()).toHaveProperty('agility');
+}); 
+
+  // Test 3 - 
+test('gets inventpry from player or returns false', () => {
+  const player = new Player('Dave'); 
+
+  expect(player.getInventory()).toEqual(expect.any(Array)); 
+
+  player.inventory = []; 
+
+  expect(player.getInventory()).toEqual(false);
+})
